7375c52d244a8ee6942ad6228110d08d4b26f8dc
codebyhannah <firekatburns@gmail.com>
2024-02-28 13:25:00 -0700

    completed prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,16 @@
 using System;
+using System.Runtime.Versioning;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Clear();
+        Console.Write("What is your first name? ");
+        string first = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string last = Console.ReadLine();
+        Console.WriteLine();
+        Console.WriteLine($"Your name is {last}, {first} {last}.");
     }
 }
