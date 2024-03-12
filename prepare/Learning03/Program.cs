4a37cf18c7592801331b9a98b885c69a1c3da3ee
codebyhannah <firekatburns@gmail.com>
2024-03-12 16:01:09 -0600

    Completed Week03 learning activity fractions.

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Console.Clear();
+
+        Fraction fract1 = new Fraction();
+        Console.WriteLine(fract1.GetFractionString());
+        Console.WriteLine(fract1.getDecimalValue());
+
+        Fraction fract2 = new Fraction(5);
+        Console.WriteLine(fract2.GetFractionString());
+        Console.WriteLine(fract2.getDecimalValue());
+
+        Fraction fract3 = new Fraction(3, 4);
+        Console.WriteLine(fract3.GetFractionString());
+        Console.WriteLine(fract3.getDecimalValue());
+
+        Fraction fract4 = new Fraction(1, 3);
+        Console.WriteLine(fract4.GetFractionString());
+        Console.WriteLine(fract4.getDecimalValue());
+        
+        Console.WriteLine();
     }
 }
